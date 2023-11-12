1088ea6cc22363f31e79effc3a01652569069131
Vitor Rebula <devrebula@gmail.com>
2023-11-12 01:25:05 -0300

    home pronto

--- script.js
+++ script.js
@@ -4,4 +4,22 @@ let navbar = document.querySelector('.header .navbar');
 menu.onclick = () => {
     menu.classList.toggle('fa-times'); //vai dar a classe fa-times para o menu, que faz o "X, que chama fa-times na lib"
     navbar.classList.toggle('active'); //vai dar a classe active para o navbar, que faz o "polígono cheio"
-}
+};
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times'); //vai dar a classe fa-times para o menu, que faz o "X, que chama fa-times na lib"
+    navbar.classList.remove('active'); //vai dar a classe active para o navbar, que faz o "polígono cheio"
+};
+
+//swiper
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor : true,
+    loop: true,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
